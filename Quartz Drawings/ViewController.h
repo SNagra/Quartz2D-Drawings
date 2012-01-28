1624b10c8a827f9a8758d23595bd85d40b92dd23
Saranpreet Nagra <snagra@slyceapps.com>
2012-01-28 15:48:16 -0800

    commit 2

---
+++ Quartz Drawings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quartz Drawings
+//
+//  Created by Saranpreet Nagra on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Quartz Drawings/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quartz Drawings
+//
+//  Created by Saranpreet Nagra on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
