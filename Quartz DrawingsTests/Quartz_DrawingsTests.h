3606428f3b3b6f333a4da3dd271268ea0facd0a1
Saranpreet Nagra <snagra@slyceapps.com>
2012-01-28 15:48:00 -0800

    Initial commit

---
+++ Quartz DrawingsTests/Quartz_DrawingsTests.h
@@ -0,0 +1,13 @@
+//
+//  Quartz_DrawingsTests.h
+//  Quartz DrawingsTests
+//
+//  Created by Saranpreet Nagra on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Quartz_DrawingsTests : SenTestCase
+
+@end
---
+++ Quartz DrawingsTests/Quartz_DrawingsTests.m
@@ -0,0 +1,32 @@
+//
+//  Quartz_DrawingsTests.m
+//  Quartz DrawingsTests
+//
+//  Created by Saranpreet Nagra on 1/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Quartz_DrawingsTests.h"
+
+@implementation Quartz_DrawingsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Quartz DrawingsTests");
+}
+
+@end
